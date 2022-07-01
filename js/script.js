7671703c72178c2b996062c5efe82bf2602ae44f
Jakub Plecháč <loxtox74@gmail.com>
2022-07-01 13:16:26 +0200

    added glider

--- js/script.js
+++ js/script.js
@@ -22,3 +22,35 @@ const removeShadow = () => {
   const element = document.getElementById("box_sh");
   element.classList.remove("box_s");
 };
+
+$(".sec2_home_slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: false,
+  autoplay: true,
+  autoplaySpeed: 4000,
+  speed: 750,
+  prevArrow: '<i class="fa-solid fa-chevron-left"></i>',
+  nextArrow: '<i class="fa-solid fa-chevron-right"></i>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
