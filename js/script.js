2e9452f88c7f420041f2493e337a3c8657f5b1bc
Jakub Plecháč <plechac@spstrutnov.cz>
2023-02-22 18:56:06 +0100

    removed jquery and slick.js

--- js/script.js
+++ js/script.js
@@ -8,32 +8,6 @@ window.addEventListener(
   {}
 );
 
-// Passive event listeners
-jQuery.event.special.touchstart = {
-  setup: function (_, ns, handle) {
-    this.addEventListener("touchstart", handle, {
-      passive: !ns.includes("noPreventDefault"),
-    });
-  },
-};
-jQuery.event.special.touchmove = {
-  setup: function (_, ns, handle) {
-    this.addEventListener("touchmove", handle, {
-      passive: !ns.includes("noPreventDefault"),
-    });
-  },
-};
-jQuery.event.special.wheel = {
-  setup: function (_, ns, handle) {
-    this.addEventListener("wheel", handle, { passive: true });
-  },
-};
-jQuery.event.special.mousewheel = {
-  setup: function (_, ns, handle) {
-    this.addEventListener("mousewheel", handle, { passive: true });
-  },
-};
-
 // Sidebar
 const openNav = () => {
   document.getElementById("mySidebar").style.left = "0px";
@@ -64,38 +38,3 @@ const removeShadow = () => {
   const element = document.getElementById("box_sh");
   element.classList.remove("box_s");
 };
-
-// Slide
-$(".sec2_home_slider").slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  dots: false,
-  autoplay: true,
-  autoplaySpeed: 4000,
-  speed: 750,
-  prevArrow:
-    '<img class="previous-arrow" src="icons/chevron-left-solid.svg" alt="Previous arrow" />',
-  nextArrow:
-    '<img class="next-arrow" src="icons/chevron-right-solid.svg" alt="Next arrow" />',
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-  ],
-});
