25c595d004bc57a270a77bafc0b454e5c9d3cb3a
Jakub Plecháč <plechac.k@gmail.com>
2023-11-19 20:29:39 +0100

    api key in .env file

--- api/getApiKey.js
+++ api/getApiKey.js
@@ -1,4 +1,4 @@
-const apiKey = "189b7af7-1846-4b47-9b90-3105397a47ee";
+const apiKey = process.env.API_KEY || "default_api_key";
 
 module.exports = (req, res) => {
   res.status(200).json({ apiKey });
